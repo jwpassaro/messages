09518c2a1523111016d75f1e82038ca8a284e0c7
Jennifer Passaro <jennifer.wheatley@gmail.com>
2015-02-11 20:42:23 -0500

    sql

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,14 @@
 class MessagesController < ApplicationController
   def show
-    @messages = Array(session[:messages])
-    render "messages/show"
+    @messages = Message.all
   end
 
   def create
-    @messages = Array(session[:messages])
-    @messages << params.merge("timestamp" => Time.now)
-    session[:messages] = @messages
+    Message.create(
+      username: params[:username],
+      message: params[:message]
+      )
     redirect_to "/messages"
   end
+
 end
