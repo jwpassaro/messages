58fd6fb48c3768316eb41229c1e52a0d086fe08c
Jennifer Passaro <jennifer.wheatley@gmail.com>
2015-02-16 20:40:42 -0500

    addingvalidation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  around_filter :set_time_zone
+
+  def set_time_zone(&block)
+    Time.use_zone("Eastern Time (US & Canada)", &block)
+  end
 end
